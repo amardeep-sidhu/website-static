e56e08542148b61daf862c51f8af03077b95ae87
amardeep-sidhu <amardeep-sidhu@users.noreply.github.com>
2024-12-28 12:32:09 +0000

    deploy: amardeep-sidhu/website-source@1e70880fc73af5cd7a076b4253fd484bf0ebdb7f

---
+++ wp-content/uploads/sucuri/sucuri-ignorescanning.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=ignorescanning;
+// created_on=1723125659;
+// updated_on=1723125659;
+exit(0);
+?>
---
+++ wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -0,0 +1,2 @@
+<?php exit(0); ?>
+{"user_id":1,"user_login":"admin","user_remoteaddr":"180.195.207.52","user_hostname":"180.195.207.52","user_lastlogin":"2024-08-09 13:08:25"}
